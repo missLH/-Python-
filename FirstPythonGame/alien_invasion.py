6705a38605d76888a03d130a9fdd2095eba6093a
czr <303170942@qq.com>
2018-11-19 18:36:36 +0800

    1b

--- FirstPythonGame/alien_invasion.py
+++ FirstPythonGame/alien_invasion.py
@@ -6,12 +6,12 @@ from settings import Settings
 def run_game():
     #初始化游戏并创建一个屏幕对象
     pygame.init()
-
-    screen = pygame.display.set_mode((1200, 800))
+    # 创建一个Settings实例
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height ))
     pygame.display.set_caption('Alien Invasion')
 
-    #set the bg color
-    bg_color = (210, 210, 210)
+
 
     # 开始游戏的主循环
     while True:
@@ -21,7 +21,7 @@ def run_game():
                 sys.exit()
 
         # 每次循环都重新绘制屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         # 让最近绘制的屏幕可见，每次执行while循环时都绘制一个空屏幕并擦去旧的，起到不断更新屏幕的作用
         pygame.display.flip()
